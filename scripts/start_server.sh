39b56959fb75d352763ec73de7fff33766e7ecaf
vikram390 <112178932+vikram390@users.noreply.github.com>
2024-09-18 06:28:27 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/my-node-app
 # Kill any existing server process
-# pkill -f node 
+pkill -f node 
 # Start the Node.js application
 # node app.js > app.log 2>&1 &
 node app.js
