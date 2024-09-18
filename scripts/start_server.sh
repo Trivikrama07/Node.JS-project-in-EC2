befd87f72bdd514048b4d2d3f3f8ae5481fe9c8d
vikram390 <112178932+vikram390@users.noreply.github.com>
2024-09-18 07:10:20 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -9,8 +9,7 @@ pkill node
 
 # Start the Node.js application in the background and redirect logs
 echo "Starting the Node.js application..."
-# nohup node app.js > app.log 2>&1 &
-node app.js
+nohup node app.js 
 
 # Check if the application started successfully
 if [[ $? -eq 0 ]]; then
